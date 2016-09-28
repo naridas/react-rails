df6c789bf5079064b18bc3b9ce614d0836131ef6
naridas <gregsparta@gmail.com>
2016-09-28 17:00:08 +0100

    changes to route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  resources :events
-  get 'dashboard/index'
+  root 'dashboard#index'
+  namespace :api do
+    resources :events
+  end
+
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
